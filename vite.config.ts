92681f51bd7a654383d3ddbb67b93860f4f87cf1
Muhammad Usama <m.usama.liaqat@gmail.com>
2024-11-21 01:37:01 +0500

    update project for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  if (mode === 'gh-pages') {
+    return {
+      plugins: [react()],
+      base: '/ice-server-test/',
+    };
+  }
+  return {
+    plugins: [react()],
+  };
+});
